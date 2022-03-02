da5e2199373f521029bc6a7bdb6b126505bf99ef
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-02 23:57:44 +0100

    created index.jsx, now rendering

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+/** Kobler denne opp mot index.html fil  **/
+ReactDOM.render(
+    <h1>React Quiz</h1>,
+    document.getElementById("app")
+);
