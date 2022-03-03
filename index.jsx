f2517e2cbe56a33b693a473821e23557a5d63733
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-03 01:50:51 +0100

    added questions.js and functional components in index.jsx

--- index.jsx
+++ index.jsx
@@ -1,8 +1,52 @@
 import * as React from "react";
+import {useState} from "react";
 import * as ReactDOM from "react-dom";
+
+import {randomQuestion, isCorrectAnswer} from "./questions";
+
+// Tar inn question-funksjonen som parameter
+function ShowQuestion({question, onAnswer}) {
+    return <>
+        <h1>{question.question}</h1>
+        {Object.keys(question.answers)   /**henter ut med key (som er svarene)**/
+            .filter(a => question.answers[a])   /** filtrere ut hvert answer i question **/
+            .map(a => <p key={a}>
+                <button onClick={() => onAnswer(a)}>{question.answers[a]}</button>
+            </p>)}
+
+    </>;
+}
+
+function ShowAnswerStatus({answer, onRestart, question}) {
+    return<>
+        <h1>{isCorrectAnswer(question, answer) ? "Right" : "Wrong"}</h1>
+        <p>
+            <button onClick={onRestart}>Another question</button>
+        </p>
+    </>;
+}
+
+function Application() {
+    /** Henter funksjonene, og setter verdier **/
+    const [question, setQuestion] = useState(randomQuestion());
+    const [answer, setAnswer] = useState();
+
+    /** Når applikasjonen starter får vi et random spørsmål og ingen svar **/
+    function handleRestart() {
+        setQuestion(randomQuestion());
+        setAnswer(undefined);
+    }
+
+    if(answer) {
+        return <ShowAnswerStatus question={question} answer={answer} onRestart={handleRestart}/>;
+    }
+
+    return <ShowQuestion question={question} onAnswer={setAnswer}/>;
+}
 
 /** Kobler denne opp mot index.html fil  **/
 ReactDOM.render(
-    <h1>React Quiz</h1>,
+    <Application/>,
     document.getElementById("app")
 );
+
