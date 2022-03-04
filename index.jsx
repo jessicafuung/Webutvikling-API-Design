dd0e1b77d375041c5dbc214e3a5b645735a140bf
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-04 03:50:19 +0100

    create new movie

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function FrontPage() {
         return <div>
@@ -22,6 +23,11 @@ const MOVIES = [
         title: "Jessica koder",
         plot: "Husker jeg dette senere?",
         year: 2022
+    },
+    {
+        title: "Notat",
+        plot: "En av to måter å bruke state/ tilstand på",
+        year: 2022
     }
 ]
 
@@ -34,18 +40,59 @@ function MovieCard(props) {
 }
 
 function ListMovies() {
-
-
     return <div>
         <h1>Movies</h1>
         {MOVIES.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
     </div>
 }
 
+function CreateMovies() {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    const [newMovie, setNewMovie] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setNewMovie({title, year, plot})
+    }, [title, year, plot]);
+
+    console.log("Render");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        MOVIES.push(newMovie);
+        navigate("..");
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <h1>Create new movie</h1>
+        <div>
+            Title:
+            <input value={title} onChange={ e => setTitle(e.target.value)}/>
+        </div>
+        <div>
+            Year:
+            <input value={year} onChange={ e => setYear(e.target.value)}/>
+        </div>
+        <div>
+            <div>Plot:</div>
+            <textarea value={plot} onChange={ e => setPlot(e.target.value)}/>
+        </div>
+
+        <button>Save</button>
+
+        <pre>
+            {JSON.stringify(newMovie)}
+        </pre>
+    </form>
+}
+
 function MovieApplication() {
     return <Routes>
                 <Route path={"/"} element={<ListMovies />} />
-                <Route path={"/new"} element={<h1>Create new movie</h1>} />
+                <Route path={"/new"} element={<CreateMovies/>} />
                 <Route path={"*"} element={<h1>Not found</h1>} />
     </Routes>
 }
