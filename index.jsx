afe8fd939947101e1d51b31a155d4d1352fce635
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-04 02:07:50 +0100

    list movies with MovieCard per movie

--- index.jsx
+++ index.jsx
@@ -12,10 +12,41 @@ function FrontPage() {
         </div>;
 }
 
+const MOVIES = [
+    {
+        title: "Dont look up",
+        plot: "I dont know",
+        year: 2021
+    },
+    {
+        title: "Jessica koder",
+        plot: "Husker jeg dette senere?",
+        year: 2022
+    }
+]
+
+function MovieCard(props) {
+    const {title, plot, year} = props.movie;
+    return <div>
+        <h2>{title} ({year})</h2>
+        <p>{plot}</p>
+    </div>;
+}
+
+function ListMovies() {
+
+
+    return <div>
+        <h1>Movies</h1>
+        {MOVIES.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
+    </div>
+}
+
 function MovieApplication() {
     return <Routes>
-                <Route path={"/"} element={<h1>Movies</h1>} />
+                <Route path={"/"} element={<ListMovies />} />
                 <Route path={"/new"} element={<h1>Create new movie</h1>} />
+                <Route path={"*"} element={<h1>Not found</h1>} />
     </Routes>
 }
 
@@ -23,7 +54,7 @@ function Application() {
         return <BrowserRouter>
                 <Routes>
                     <Route path={"/"} element={<FrontPage />} />
-                    <Route path={"/movies/*"} element={MovieApplication} />
+                    <Route path={"/movies/*"} element={<MovieApplication />} />
                     <Route path={"*"} element={<h1>Not found</h1>} />
                 </Routes>
         </BrowserRouter>
