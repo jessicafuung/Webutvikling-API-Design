793f01232f65221dd951d5b419dfd086eb5b968c
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-04 14:10:04 +0100

    Minimal jest snapshot test

---
+++ __test__/movie.test.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+import {ListMovies} from "../application";
+
+
+describe("movies application", () => {
+
+    it("shows movie list", () => {
+        const element = document.createElement("div");
+        ReactDOM.render(<ListMovies />, element)
+
+        expect(element.querySelector("h1").innerHTML)
+            .toEqual("List movies");
+        expect(element.innerHTML).toMatchSnapshot();
+    })
+
+})
