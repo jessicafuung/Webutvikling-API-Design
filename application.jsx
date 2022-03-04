909d8c09ed694e8b63447055e2ade81ffed921b7
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-04 16:41:17 +0100

    simulated test

--- application.jsx
+++ application.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 export function ListMovies() {
     return <div><h1>List movies</h1>
@@ -6,21 +6,29 @@ export function ListMovies() {
             <li>Movie 1</li>
             <li>Movie 2</li>
         </ul>
-
     </div>;
 }
 
-export function NewMovieForm() {
-    return <form>
+export function NewMovieForm({onAddMovie}) {
+
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit() {
+        onAddMovie({title, year, plot})
+    }
+
+    return <form onSubmit={handleSubmit}>
         <h1>Add new movie</h1>
         <div>
-            Title: <input/>
+            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
         <div>
-            Year: <input/>
+            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)}/>
         </div>
         <div>
-            Plot: <textarea/>
+            Plot: <textarea data-testid="plot"/>
         </div>
     </form>
 }
