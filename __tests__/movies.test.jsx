b0bf8389d6767a1b098898f85c76919d3960a8e9
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-04 04:31:00 +0100

    include babel-jest so that jest can use react c ode

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -1,7 +1,11 @@
+import {render} from "react-dom";
+import {ListMovies} from "../index";
 
 describe("movie pages", () => {
 
     it("shows movie list", () => {
+        const element = document.createElement("div");
+        render(<ListMovies />, element)
         expect("movie").toBe("movie");
     });
 
