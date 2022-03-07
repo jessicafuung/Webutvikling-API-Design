3d6ffb846f91a411a3f3e95768f5e9f2083a74bc
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-07 14:13:51 +0100

    github actions workflow file

--- application.jsx
+++ application.jsx
@@ -1,34 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export function ListMovies() {
-    return <div><h1>List movies</h1>
-        <ul>
-            <li>Movie 1</li>
-            <li>Movie 2</li>
-        </ul>
-    </div>;
+  return (
+    <div>
+      <h1>List movies</h1>
+      <ul>
+        <li>Movie 1</li>
+        <li>Movie 2</li>
+      </ul>
+    </div>
+  );
 }
 
-export function NewMovieForm({onAddMovie}) {
+export function NewMovieForm({ onAddMovie }) {
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [plot, setPlot] = useState("");
 
-    const [title, setTitle] = useState("");
-    const [year, setYear] = useState("");
-    const [plot, setPlot] = useState("");
+  function handleSubmit() {
+    onAddMovie({ title, year, plot });
+  }
 
-    function handleSubmit() {
-        onAddMovie({title, year, plot})
-    }
-
-    return <form onSubmit={handleSubmit}>
-        <h1>Add new movie</h1>
-        <div>
-            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)}/>
-        </div>
-        <div>
-            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)}/>
-        </div>
-        <div>
-            Plot: <textarea data-testid="plot"/>
-        </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add new movie</h1>
+      <div>
+        Title:{" "}
+        <input
+          data-testid="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        Year:{" "}
+        <input
+          data-testid="year"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        />
+      </div>
+      <div>
+        Plot: <textarea data-testid="plot" />
+      </div>
     </form>
+  );
 }
