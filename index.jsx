ed74c22c75d2b5228cb16dee5e352d03851602d9
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-10 17:01:14 +0100

    imported App()

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import * as ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
+import {App} from "./quiz";
 
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("app"));
