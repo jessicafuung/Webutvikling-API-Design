4f83702dc979649de29e1427db2c610e6c8cc29d
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-22 19:08:19 +0100

    test suites finished

--- index.jsx
+++ index.jsx
@@ -1,6 +1,10 @@
 import * as ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import {App} from "./quiz";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./quiz";
 
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
