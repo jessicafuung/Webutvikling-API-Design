ad0534dc36d86446c97d6395dcb6b46e9bbf440f
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-25 17:14:43 +0100

    respond to POST

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 //respond to a get request
 app.get("/login", (req, res) => {
@@ -9,8 +11,32 @@ app.get("/login", (req, res) => {
     });
 });
 
+const users = [
+    {
+        username: "admin",
+        password: "secret123"
+    }
+]
+
+//set something so that GET /login returns user name
 app.post("/login", (req, res) => {
-    //set something so that GET /login returns user name
+    // 1. read body as json
+    // 2. check if username and password is correct
+    // 3. set a cookie
+    // 4. read the cookie in /login
+
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
+
+    //Among the users, find a matching username. It's password should also match.
+    if (users.find(u => username === username).password === password) {
+        res.sendStatus(200)
+    } else {
+        //else return
+        res.send(401)
+    }
+
     res.end();
 })
 
