e22de4497d3b87542698c6cce5992a13643a6363
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-25 12:39:16 +0100

    respond to get request

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+//Svar på en get request
+app.get("/login", (req, res) => {
+    res.send({
+        username: "admin"
+    });
+});
+
 //For å lytte til en port (template literals)
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server started at http://localhost:${server.address().port}`);
