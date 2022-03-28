c254442f0d280370495541fd4abe3b2437811cbc
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-28 16:59:33 +0200

    created simple express server

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/quiz/random", (req, res) => {
+   res.json("hello")
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`server started at http://localhost:${server.address().port}`)
+});
