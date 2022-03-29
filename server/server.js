11ea9e54b7891259b19c997b129f41e4b618be9d
Jessica Fung <jessica-fcc@hotmail.com>
2022-03-29 19:29:13 +0200

    Update README.md

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res) => {
-    res.json({username: "Noen andre"})
-})
+  res.json({ username: "Noen andre" });
+});
 
 app.post("/login", (req, res) => {
-    res.sendStatus(401);
-})
+  res.sendStatus(401);
+});
 
-const server = app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`started on http://localhost:${server.address().port}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`started on http://localhost:${server.address().port}`);
 });
