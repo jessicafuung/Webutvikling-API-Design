1a5ef9153f4ca889571019550190bb2b9d32447e
Jessica Fung <jessica-fcc@hotmail.com>
2022-04-01 00:52:02 +0200

    Use React Router in client (improved middleware)

--- server/server.js
+++ server/server.js
@@ -12,8 +12,14 @@ app.post("/api/login", (req, res) => {
 });
 
 app.use(express.static("../client/dist"));
+
+// denne skal serve riktig html fil
 app.use((req, res, next) => {
-  res.sendFile(path.resolve("../client/dist/index.html"));
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
