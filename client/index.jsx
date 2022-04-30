f3fe314a628eabc61fca285bd04d8d7f5a0c693e
Jessica Fung <jessica-fcc@hotmail.com>
2022-04-30 22:51:54 +0200

    Load user information from server in FrontPage

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+{
+  /*
+        Vet ikke hvem brukeren er:
+        - Lurt å ha <div> rundt linkene at de skal være hver for seg på web
+   */
+}
+function LoginLinks() {
+  return (
+    <div>
+      <div>
+        <Link to={"/login"}>Login</Link>
+      </div>
+      <div>
+        <Link to={"/register"}>Register now</Link>
+      </div>
+    </div>
+  );
+}
+
+{
+  /*
+        Forside:
+        - Viser kun <LoginLinks/> hvis vi ikke vet hvem brukeren er
+        - UseEffect hook kaller fetch-kallet - som da gjør en HTTP request. *fetch er asynkront *await lar koden gjør andre ting i mellomtiden
+        - useState for å lagre user-objektet (denne må importeres fra react)
+        - i return: hvis user er satt, returner navnet til brukeren. Hvis ikke, vis <LoginLinks/>
+  */
+}
+function FrontPage() {
+  const [user, setUser] = useState();
+  useEffect(async () => {
+    const res = await fetch("api/login");
+    setUser(await res.json());
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie Application</h1>
+      {user ? <div>{user.fullName}</div> : <LoginLinks />}
+    </div>
+  );
+}
+
+{
+  /*
+  Logg inn:
+  */
+}
+function Login() {
+  return <h1>Welcome to login page</h1>;
+}
+
+{
+  /* URL som linker til gjeldende function components */
+}
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.get("/api/login", (req, res) => {
-  res.json({
-    username: "admin",
-    fullName: "Noen Andre Persson",
-  });
+  function respond() {
+    res.json({
+      username: "admin",
+      fullName: "Noen Andre Persson",
+    });
+  }
+  setTimeout(respond, 3000);
 });
 
+// de to app.use under brukes til å serve React kode via Express!
+// statiske filer (html, css, etc)
+app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    // hvis ikke den getter /api/.., returner index.html fil
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    //404 handler
+    next();
+  }
+});
+
+// Lytte til port
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server started on http://localhost:${server.address().port}`);
 });
