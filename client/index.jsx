2fc67ab076870f5751f8bb6b989ac347061bd51a
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-02 00:30:34 +0200

    List movies on the client side * useLoader * fetchJSON function

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
@@ -18,10 +18,75 @@ function FrontPage() {
   );
 }
 
+/* Custom hook */
+function useLoading(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  /* denne funksjonen kunne man ha satt direkte inn i useeffect. Men grunner til å ikke gjøre det:
+   * 1. sette den inn i return statement, for å kalle på den uten ifra
+   * 2. på typescript kan man ikke kalle på async fra useEffect, derfor egen funksjon.
+   * */
+  async function load() {
+    try {
+      /* i ferd med å laste */
+      setLoading(true);
+      /* setData er resultatet av loadingFunction, venter på den */
+      setData(await loadingFunction());
+    } catch (error) {
+      /* men hvis det skjer en feil, send den inn i setError */
+      setError(error);
+    } finally {
+      /* til slutt ikke load mer */
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return { loading, error, data };
+}
+
+async function fetchJSON(url) {
+  /* vente på å få fetche API fra server siden */
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
+  }
+  /* vente på å parse til json */
+  return await res.json();
+}
+
 function ListMovies() {
+  /* Er den i ferd med å laste? Fått noe feil? Eller data? Husk å send inn en loadingFunction som fetcher api */
+  const { loading, error, data } = useLoading(async () =>
+    fetchJSON("/api/movies")
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Movies in the database</h1>
+      <ul>
+        {data.map((movie) => (
+          <li>{movie.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
