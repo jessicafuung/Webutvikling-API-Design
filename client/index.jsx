18a7760501b56bc1d70a49c23e7738c6762390b7
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-02 17:10:59 +0200

    React Router with FrontPage,Profile, Login and LoginCallback

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Heloooooo</h1>, document.getElementById("app"))
+function FrontPage() {
+    return <div>
+        <h1>FrontPage</h1>
+        <div>
+            <Link to={"/login"}>Login</Link>
+        </div>
+        <div>
+            <Link to={"/profile"}>Profile</Link>
+        </div>
+    </div>
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage/>} />
+            <Route path={"/login"} element={<h1>Login</h1>} />
+            <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
+            <Route path={"/profile"} element={<h1>Profile</h1>} />
+        </Routes>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"))
