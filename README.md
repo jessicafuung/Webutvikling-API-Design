ed60b71f000992778bf851c59520b007b2797946
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-02 19:33:26 +0200

    Login component redirect to login with Google, changed react version to 17

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 https://pg6301-08.herokuapp.com
 
+https://accounts.google.com/.well-known/openid-configuration
+
+https://console.cloud.google.com/getting-started?hl=NO&supportedpurview=project
+
 * [x] Create Express + React project (https://github.com/kristiania-pg6301-2022/pg6301-react-and-express-lectures#quickly-creating-a-express--react-application)
 * [x] Create server.js and index.html
 * [x] Routes: FrontPage, Login, LoginCallback, Profile
--- client/index.jsx
+++ client/index.jsx
@@ -1,28 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
-    return <div>
-        <h1>FrontPage</h1>
-        <div>
-            <Link to={"/login"}>Login</Link>
-        </div>
-        <div>
-            <Link to={"/profile"}>Profile</Link>
-        </div>
+  return (
+    <div>
+      <h1>FrontPage</h1>
+      <div>
+        <Link to={"/login"}>Login</Link>
+      </div>
+      <div>
+        <Link to={"/profile"}>Profile</Link>
+      </div>
     </div>
+  );
+}
+
+/* Bruker denne vente funksjonen til å fetche det som er i google doc, for å unngå hardkode endpoints */
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+
+  return await res.json();
+}
+
+function Login() {
+  const [redirectUrl, setRedirectUrl] = useState();
+
+  /* Redirecte til en ny side ved trykk på "do login", og ikke automatisk inne på /login */
+  useEffect(async () => {
+    /* "authorization_endpoint" på google JSON. Ønsker å fetche istedenfor å hardkode. Destruction syntax for å hente ut det vi ønsker med JSON */
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+
+    const parameters = {
+      /* response_type: ønsker å få en token tilbake */
+      /* client_id: hentes fra google developer console */
+      /* scope: hva slags informasjon ønsker jeg å få tilbake om brukeren */
+      /* redirect_uri: send brukeren tilbake til denne application, men denne url'n */
+      response_type: "token",
+      client_id:
+        "775167009240-u1lscab9fno21qd3e1pd0ihf194aq6hn.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+
+    /* URLSearchParams = gjør om fra object til JSON*/
+    setRedirectUrl(
+      authorization_endpoint + "?" + new URLSearchParams(parameters)
+    );
+  }, []);
+
+  return (
+    <div>
+      <h1>Login updated!</h1>
+      <a href={redirectUrl}>Do login</a>
+      <div>{redirectUrl}</div>
+    </div>
+  );
 }
 
 function Application() {
-    return <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<FrontPage/>} />
-            <Route path={"/login"} element={<h1>Login</h1>} />
-            <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
-            <Route path={"/profile"} element={<h1>Profile</h1>} />
-        </Routes>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/login/callback"} element={<h1>Login Callback</h1>} />
+        <Route path={"/profile"} element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-ReactDOM.render(<Application/>, document.getElementById("app"))
+ReactDOM.render(<Application />, document.getElementById("app"));
