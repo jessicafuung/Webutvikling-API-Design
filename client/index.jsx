d986835274e561e3ca1fc57de5b89f284e17a5c4
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-02 23:15:16 +0200

    Fetch userinfo from Google and display in <Profile/> with image

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 function FrontPage() {
   return (
@@ -59,7 +65,10 @@ function Login() {
   );
 }
 
+/* denne callback er mest for å vente på useEffect skal bli ferdig. Bruk useNavigate til å navigere seg vekk når ferdig */
 function LoginCallback() {
+  const navigate = useNavigate();
+
   /* henter ut token etter login med en useEffect og destruction syntax */
   useEffect(async () => {
     /* viktig å hoppe over første tegnet '#', hvis ikke blir det undefined */
@@ -76,9 +85,70 @@ function LoginCallback() {
       },
       body: JSON.stringify({ access_token }),
     });
+
+    /* naviger tilbake til forsiden når ferdig */
+    navigate("/");
   });
 
-  return <h1>Login Callback</h1>;
+  return <h1>Please wait...</h1>;
+}
+
+/* custom hook */
+function useLoader(loadingFn) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  /* denne funksjonen kunne man ha satt direkte inn i useeffect. Men grunner til å ikke gjøre det:
+   * 1. sette den inn i return statement, for å kalle på den uten ifra
+   * 2. på typescript kan man ikke kalle på async fra useEffect, derfor egen funksjon.
+   * */
+  async function load() {
+    try {
+      /* i ferd med å laste */
+      setLoading(true);
+      /* setData er resultatet av loadingFunction, venter på den */
+      setData(await loadingFn());
+    } catch (error) {
+      /* men hvis det skjer en feil, send den inn i setError */
+      setError(error);
+    } finally {
+      /* til slutt ikke load mer */
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return { loading, error, data };
+}
+
+function Profile() {
+  const { loading, data, error } = useLoader(async () => {
+    return await fetchJSON("/api/login");
+  });
+
+  if (loading) {
+    return <div>Please wait...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.toString()}</div>;
+  }
+
+  return (
+    <div>
+      <h1>
+        Profile for {data.name} ({data.email})
+      </h1>
+      <div>
+        <img src={data.picture} alt={"Profile picture"} />
+      </div>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
 }
 
 function Application() {
@@ -88,7 +158,7 @@ function Application() {
         <Route path={"/"} element={<FrontPage />} />
         <Route path={"/login"} element={<Login />} />
         <Route path={"/login/callback"} element={<LoginCallback />} />
-        <Route path={"/profile"} element={<h1>Profile</h1>} />
+        <Route path={"/profile"} element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- server/server.js
+++ server/server.js
@@ -4,11 +4,47 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
+import fetch from "node-fetch";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+/* Bruker denne vente funksjonen til å fetche det som er i google doc, for å unngå hardkode endpoints */
+// viktig å importere fetch fra node-fetch
+// legg til options (autorisasjon med token)
+async function fetchJSON(url, options) {
+  const res = await fetch(url, options);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+
+  return await res.json();
+}
+
+// async pga await inni fetchen
+app.get("/api/login", async (req, res) => {
+  // henter access token fra cookies, som ble hentet ifra #object
+  // da må serveren finne ut hvem er dette
+  const { access_token } = req.signedCookies;
+
+  // bruke funksjonen som vi lagde i index.jsx, til å lete gjennom google docen
+  const { userinfo_endpoint } = await fetchJSON(
+    "https://accounts.google.com/.well-known/openid-configuration"
+  );
+
+  // hente userinfo via endpoint fra google doc
+  // må sende inn token for å få hentet riktig info
+  const userinfo = await fetchJSON(userinfo_endpoint, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  //response
+  res.json(userinfo);
+});
 
 // tar imot access token fra <LoginCallback/> og bruker den til å sette cookien.
 // denne trenger egentlig ikke å være signed fordi den er autorisert fra Google allerede
